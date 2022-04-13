6aa0b1c37af10524aa6c63011e769189c811eabb
Vikas Bahuguna <vikasedu10@gmail.com>
2022-04-13 06:19:09 +0000

    Update Jenkinsfile for multibranch pipeline (master- test,build,deploy), (other branch-test)

--- script.groovy
+++ script.groovy
@@ -1,14 +1,14 @@
-def buildApp() {
-    echo "Building your application"
-}
-
 def testApp() {
     echo "Testing the application"
 }
 
+def buildApp() {
+    echo "Building your application"
+}
+
 def deployApp() {
     echo "Deploying your application"
-    echo "Application version to be deployed: ${VERSION}"
+    echo "Application version to be deployed is latest by default"
 }
 
 return this
